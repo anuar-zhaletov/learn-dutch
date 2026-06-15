8bf3e8c92adab8cb8b5c315d5ae8ad907deb327c
anuarzhaletov <anuar.zhaletov@icscards.nl>
2026-06-15 20:44:25 +0200

    words

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,8 @@
-void main() {
-    System.out.println("Learn Dutch!");
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+void main() throws Exception {
+    try (var lines = Files.lines(Path.of("resources/words-working.csv"))) {
+        lines.forEach(System.out::println);
+    }
 }
